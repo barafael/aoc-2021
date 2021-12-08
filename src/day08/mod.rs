0426f6f902525393b3ecfc2fe7c6cec6c4644f7d
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-12-08 21:23:01 +0100

    Add solutions for day 8

---
+++ src/day08/mod.rs
@@ -0,0 +1,69 @@
+use std::str::FromStr;
+
+pub mod problem_1;
+pub mod problem_2;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Entry {
+    patterns: Vec<String>,
+    output: Vec<String>,
+}
+
+impl FromStr for Entry {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tokens = s.split_whitespace().collect::<Vec<_>>();
+        if tokens.get(10).ok_or(())? != &"|" {
+            return Err(());
+        }
+        if tokens.len() != 15 {
+            return Err(());
+        }
+        let tokens = &mut tokens.iter();
+        let patterns = tokens
+            .take(10)
+            .cloned()
+            .map(String::from)
+            .collect::<Vec<_>>();
+        let _pipe = tokens.next().ok_or(())?;
+        let output = tokens
+            .take(4)
+            .cloned()
+            .map(String::from)
+            .collect::<Vec<_>>();
+        Ok(Entry { patterns, output })
+    }
+}
+
+pub fn parse_to_vec(input: &str) -> Vec<Entry> {
+    input
+        .trim()
+        .lines()
+        .map(Entry::from_str)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::parse_to_vec;
+
+    pub const INPUT: &str = include_str!("../../input/day08.txt");
+    pub const EXAMPLE_INPUT: &str =
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+
+    #[test]
+    fn parses_input_ok() {
+        parse_to_vec(INPUT);
+    }
+}
---
+++ src/day08/problem_1.rs
@@ -0,0 +1,39 @@
+use super::Entry;
+
+pub fn count_1_4_7_8(entries: &[Entry]) -> usize {
+    entries
+        .iter()
+        .map(|entry| {
+            entry
+                .output
+                .iter()
+                .filter(|output| {
+                    output.len() == 2 || output.len() == 3 || output.len() == 4 || output.len() == 7
+                })
+                .count()
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day08::{
+        parse_to_vec,
+        problem_1::count_1_4_7_8,
+        test::{EXAMPLE_INPUT, INPUT},
+    };
+
+    #[test]
+    fn computes_26_instances_in_example_input() {
+        let entries = parse_to_vec(EXAMPLE_INPUT);
+        let sum = count_1_4_7_8(&entries);
+        assert_eq!(26, sum);
+    }
+
+    #[test]
+    fn computes_sum_of_1_4_7_8() {
+        let entries = parse_to_vec(INPUT);
+        let sum = count_1_4_7_8(&entries);
+        assert_eq!(543, sum);
+    }
+}
