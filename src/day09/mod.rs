229135435a42e82268089604e051694bf14116ad
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-12-10 00:03:32 +0100

    Add solution for day 9 problem 2

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -13,6 +13,81 @@ pub fn parse_to_vec_vec(input: &str) -> Vec<Vec<u8>> {
         .collect::<Vec<_>>()
 }
 
+pub fn find_low_points(height_map: &[Vec<u8>]) -> Vec<(usize, usize)> {
+    let max_depth_index = height_map.len() - 1;
+    // assumes all lines have equal length and there is at least one line.
+    let max_len_index = height_map[0].len() - 1;
+    let mut low_points = vec![];
+    // inner area.
+    for i in 1..max_depth_index {
+        for j in 1..max_len_index {
+            let value = height_map[i][j];
+            if value < height_map[i - 1][j]
+                && value < height_map[i][j + 1]
+                && value < height_map[i + 1][j]
+                && value < height_map[i][j - 1]
+            {
+                low_points.push((i, j));
+            }
+        }
+    }
+    for j in 1..max_len_index {
+        // north row.
+        let value = height_map[0][j];
+        if value < height_map[0][j + 1] && value < height_map[1][j] && value < height_map[0][j - 1]
+        {
+            low_points.push((0, j));
+        }
+        // south row.
+        let value = height_map[max_depth_index][j];
+        if value < height_map[max_depth_index][j + 1]
+            && value < height_map[max_depth_index - 1][j]
+            && value < height_map[max_depth_index][j - 1]
+        {
+            low_points.push((max_depth_index, j));
+        }
+    }
+    for i in 1..max_depth_index {
+        // west column.
+        let value = height_map[i][0];
+        if value < height_map[i - 1][0] && value < height_map[i][1] && value < height_map[i + 1][0]
+        {
+            low_points.push((i, 0));
+        }
+        // east column.
+        let value = height_map[i][max_len_index];
+        if value < height_map[i - 1][max_len_index]
+            && value < height_map[i][max_len_index - 1]
+            && value < height_map[i + 1][max_len_index]
+        {
+            low_points.push((i, max_len_index));
+        }
+    }
+    // top left.
+    let value = height_map[0][0];
+    if value < height_map[1][0] && value < height_map[0][1] {
+        low_points.push((0, 0));
+    }
+    // bottom left.
+    let value = height_map[max_depth_index][0];
+    if value < height_map[max_depth_index - 1][0] && value < height_map[max_depth_index][1] {
+        low_points.push((max_depth_index, 0));
+    }
+    // bottom right.
+    let value = height_map[max_depth_index][max_len_index];
+    if value < height_map[max_depth_index - 1][max_len_index]
+        && value < height_map[max_depth_index][max_len_index - 1]
+    {
+        low_points.push((max_depth_index, max_len_index));
+    }
+    // top right.
+    let value = height_map[0][max_len_index];
+    if value < height_map[1][max_len_index] && value < height_map[0][max_len_index - 1] {
+        low_points.push((0, max_len_index));
+    }
+    low_points
+}
+
 #[cfg(test)]
 mod test {
     use super::parse_to_vec_vec;
--- src/day09/problem_2.rs
+++ src/day09/problem_2.rs
@@ -1,2 +1,108 @@
+use std::collections::{HashSet, VecDeque};
+
+pub fn expand_region(input: &[Vec<u8>], seed: (usize, usize)) -> HashSet<(usize, usize)> {
+    let mut visited = HashSet::new();
+    let mut to_visit = VecDeque::<(usize, usize)>::new();
+    to_visit.push_back(seed);
+    while let Some(v) = to_visit.pop_front() {
+        let neighbours = get_neighbours(input, v);
+        let neighbours = neighbours
+            .iter()
+            .filter(|(i, j)| input[*i][*j] != 9)
+            .filter(|c| !to_visit.contains(c))
+            .filter(|c| !visited.contains(*c))
+            .copied()
+            .collect::<Vec<(usize, usize)>>();
+        neighbours.iter().for_each(|e| to_visit.push_back(*e));
+        visited.insert(v);
+    }
+    visited
+}
+
+fn get_neighbours(input: &[Vec<u8>], seed: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut neighbours = vec![];
+    // top.
+    if seed.0 != 0 {
+        let i = seed.0 - 1;
+        neighbours.push((i, seed.1));
+    }
+    // right.
+    if seed.1 != input[0].len() - 1 {
+        let j = seed.1 + 1;
+        neighbours.push((seed.0, j));
+    }
+    // bottom.
+    if seed.0 != input.len() - 1 {
+        let i = seed.0 + 1;
+        neighbours.push((i, seed.1));
+    }
+    // right.
+    if seed.1 != 0 {
+        let j = seed.1 - 1;
+        neighbours.push((seed.0, j));
+    }
+    neighbours
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use itertools::Itertools;
+
+    use crate::day09::{
+        find_low_points, parse_to_vec_vec,
+        problem_2::get_neighbours,
+        test::{EXAMPLE_INPUT, INPUT},
+    };
+    use std::collections::HashSet;
+
+    #[test]
+    fn gets_neighbours() {
+        let input = parse_to_vec_vec(EXAMPLE_INPUT);
+        let neighbours = get_neighbours(&input, (0, 0));
+        assert_eq!(2, neighbours.len());
+        assert_eq!(vec![(0, 1), (1, 0)], neighbours);
+
+        let neighbours = get_neighbours(&input, (2, 4));
+        assert_eq!(vec![(1, 4), (2, 5), (3, 4), (2, 3)], neighbours);
+    }
+
+    #[test]
+    fn expand_region() {
+        let input = parse_to_vec_vec(EXAMPLE_INPUT);
+        let region = super::expand_region(&input, (0, 1));
+        assert_eq!(
+            [(0, 1), (0, 0), (1, 0)].into_iter().collect::<HashSet<_>>(),
+            region
+        );
+    }
+
+    #[test]
+    fn computes_example_result() {
+        let input = parse_to_vec_vec(EXAMPLE_INPUT);
+        let low_points = find_low_points(&input);
+        let result = low_points
+            .iter()
+            .copied()
+            .map(|point| super::expand_region(&input, point))
+            .sorted_by(|one, other| other.len().cmp(&one.len()))
+            .take(3)
+            .map(|s| s.len())
+            .fold(1, |acc, new| acc * new);
+        assert_eq!(1134, result);
+    }
+
+    #[test]
+    fn computes_result() {
+        let input = parse_to_vec_vec(INPUT);
+        let low_points = find_low_points(&input);
+        let result = low_points
+            .iter()
+            .copied()
+            .map(|point| super::expand_region(&input, point))
+            .sorted_by(|one, other| other.len().cmp(&one.len()))
+            .take(3)
+            .map(|s| s.len())
+            .fold(1, |acc, new| acc * new);
+        assert_eq!(736920, result);
+    }
+}
