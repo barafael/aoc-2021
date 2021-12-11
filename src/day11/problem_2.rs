0bef42c9a271e409389da7e78d3963174987876a
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-12-11 13:16:55 +0100

    Add solution for day 11 problem 2

--- src/day11/problem_2.rs
+++ src/day11/problem_2.rs
@@ -1,2 +1,19 @@
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::day11::{parse_octopi, problem_1::octopus_blinky_party, test::INPUT};
+
+    #[test]
+    fn party_with_input_until_sync() {
+        let mut party = parse_octopi(INPUT);
+        let mut index = 0;
+        loop {
+            if party.iter().all(|line| line.iter().all(|o| *o == 0)) {
+                break;
+            }
+            let (_, new_party) = octopus_blinky_party(party);
+            party = new_party;
+            index += 1;
+        }
+        assert_eq!(494, index);
+    }
+}
