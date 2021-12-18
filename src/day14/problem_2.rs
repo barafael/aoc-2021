5b307de4403f0d313c696dd3975c831e3b1cd856
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-12-18 14:47:38 +0100

    Add solution for day 14 task 2

--- src/day14/problem_2.rs
+++ src/day14/problem_2.rs
@@ -1,72 +1,211 @@
-use std::collections::HashMap;
+use itertools::Itertools;
+use std::{collections::HashMap, iter::once};
 
-pub fn apply_replacement(
-    pairs: HashMap<[u8; 2], usize>,
-    rules: &HashMap<[u8; 2], u8>,
-    char_count: &mut HashMap<u8, usize>,
-) -> HashMap<[u8; 2], usize> {
-    let mut new_pairs = HashMap::new();
-    for (pair, count) in pairs {
-        if let Some(insert) = rules.get(&pair) {
-            let key1: [u8; 2] = [pair[0], *insert];
-            let key2: [u8; 2] = [*insert, pair[1]];
-            let first = new_pairs.entry(key1).or_insert(count);
-            *first += count;
-            let second = new_pairs.entry(key2).or_insert(count);
-            *second += count;
-            char_count.entry(*insert).and_modify(|val| *val += count);
-        } else {
-            unreachable!();
+type PairMap = HashMap<char, HashMap<char, usize>>;
+
+/// Applies a rule to each pair, inserting the two resulting pairs in the new `PairMap`.
+pub fn step(pairs: PairMap, rules: &HashMap<(char, char), char>) -> PairMap {
+    // TODO simplify with entry API of hashmap
+    let mut new_pairs = PairMap::new();
+    for (fst, snds) in pairs {
+        for (snd, count) in snds {
+            let insert = if let Some(i) = rules.get(&(fst, snd)) {
+                i
+            } else {
+                if snd == '@' {
+                    if let Some(entry) = new_pairs.get_mut(&fst) {
+                        entry.insert('@', 1);
+                    } else {
+                        new_pairs.insert(fst, [('@', 1)].into_iter().collect());
+                    }
+                }
+                continue;
+            };
+            // (fst, insert)
+            if let Some(entry) = new_pairs.get_mut(&fst) {
+                if let Some(entry) = entry.get_mut(insert) {
+                    *entry += count;
+                } else {
+                    entry.insert(*insert, count);
+                }
+            } else {
+                new_pairs.insert(fst, [(*insert, count)].into_iter().collect());
+            }
+            // (insert, snd)
+            if let Some(entry) = new_pairs.get_mut(insert) {
+                if let Some(entry) = entry.get_mut(&snd) {
+                    *entry += count;
+                } else {
+                    entry.insert(snd, count);
+                }
+            } else {
+                new_pairs.insert(*insert, [(snd, count)].into_iter().collect());
+            }
         }
     }
     new_pairs
 }
 
-pub fn print_table(map: &HashMap<[u8; 2], usize>) {
-    for (entry, count) in map {
-        println!("{}{}: {}", entry[0] as char, entry[1] as char, count);
+pub fn count_occurrences(pairs: &PairMap) -> HashMap<char, usize> {
+    let mut counts = HashMap::<char, usize>::new();
+    for (char, followers) in pairs {
+        let sum = followers.iter().map(|(_, c)| *c).sum();
+        counts.insert(*char, sum);
     }
+    counts
+}
+
+pub fn calculate_score(occurrences: &HashMap<char, usize>) -> usize {
+    let max = occurrences.iter().map(|(_, count)| count).max().unwrap();
+    let min = occurrences.iter().map(|(_, count)| count).min().unwrap();
+    max - min
+}
+
+pub fn pairs_from_str(string: &str) -> PairMap {
+    let mut map = PairMap::new();
+    for (a, b) in string.chars().chain(once('@')).tuple_windows() {
+        if let Some(entry) = map.get_mut(&a) {
+            if let Some(entry) = entry.get_mut(&b) {
+                *entry += 1;
+            } else {
+                entry.insert(b, 1);
+            }
+        } else {
+            map.insert(a, [(b, 1)].into_iter().collect());
+        }
+    }
+    map
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day14::{parse_input, problem_2::apply_replacement, test::EXAMPLE_INPUT};
+    use super::{pairs_from_str, step};
+    use crate::day14::{
+        parse_input,
+        problem_2::{count_occurrences, PairMap},
+        test::{EXAMPLE_INPUT, INPUT},
+    };
     use std::collections::HashMap;
 
     #[test]
-    #[ignore]
-    fn performs_10_steps_on_example_input() {
-        let (template, rules) = parse_input(EXAMPLE_INPUT);
+    fn applies_first_step() {
+        let (initial, rules) = parse_input(EXAMPLE_INPUT);
         let rules = rules
-            .into_iter()
-            .map(|(key, value)| (key.as_bytes().try_into().unwrap(), value as u8))
+            .iter()
+            .map(|(pair, c)| ((pair.as_bytes()[0] as char, pair.as_bytes()[1] as char), *c))
             .collect::<HashMap<_, _>>();
-        let mut pairs = HashMap::<[u8; 2], usize>::new();
-        let mut counts = HashMap::<u8, usize>::new();
-        for byte_pair in template.as_bytes().windows(2) {
-            pairs
-                .entry(byte_pair.try_into().unwrap())
-                .and_modify(|v| *v += 1)
-                .or_insert(1);
-            counts
-                .entry(byte_pair[0])
-                .and_modify(|val| *val += 1)
-                .or_insert(1);
-        }
-        let b = template.chars().last();
-        counts
-            .entry(b.unwrap() as u8)
-            .and_modify(|v| *v += 1)
-            .or_insert(1);
+        let pairs = pairs_from_str(&initial);
+        let result = step(pairs, &rules);
+        let expected = [
+            ('N', [('B', 1), ('C', 1)].into_iter().collect()),
+            ('B', [('@', 1), ('C', 1)].into_iter().collect()),
+            ('H', [('B', 1)].into_iter().collect()),
+            ('C', [('N', 1), ('H', 1)].into_iter().collect()),
+        ]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+        assert_eq!(expected, result);
+    }
 
-        let mut pairs = pairs;
-        for _i in 0..10 {
-            pairs = apply_replacement(pairs, &rules, &mut counts);
+    #[test]
+    fn applies_10_steps() {
+        let (initial, rules) = parse_input(EXAMPLE_INPUT);
+        let rules = rules
+            .iter()
+            .map(|(pair, c)| ((pair.as_bytes()[0] as char, pair.as_bytes()[1] as char), *c))
+            .collect::<HashMap<_, _>>();
+        let mut pairs = pairs_from_str(&initial);
+        for _i in 1..=10 {
+            pairs = step(pairs, &rules);
+            if _i == 5 {
+                let len: usize = count_occurrences(&pairs).iter().map(|(_, s)| *s).sum();
+                assert_eq!(97, len);
+            }
         }
-        let max = dbg!(counts.values().max().unwrap());
-        let min = dbg!(counts.values().min().unwrap());
-        let _sum = pairs.values().sum::<usize>() + 1;
-        //assert_eq!(3073, sum);
+        let occ = count_occurrences(&pairs);
+        let max = occ.iter().map(|(_, count)| count).max().unwrap();
+        let min = occ.iter().map(|(_, count)| count).min().unwrap();
+        assert_eq!(1588, max - min);
+    }
+
+    #[test]
+    fn applies_40_steps() {
+        let (initial, rules) = parse_input(EXAMPLE_INPUT);
+        let rules = rules
+            .iter()
+            .map(|(pair, c)| ((pair.as_bytes()[0] as char, pair.as_bytes()[1] as char), *c))
+            .collect::<HashMap<_, _>>();
+        let mut pairs = pairs_from_str(&initial);
+        for _i in 1..=40 {
+            pairs = step(pairs, &rules);
+        }
+        let occ = count_occurrences(&pairs);
+        let max = occ.iter().map(|(_, count)| count).max().unwrap();
+        let min = occ.iter().map(|(_, count)| count).min().unwrap();
         assert_eq!(2188189693529, max - min);
+    }
+
+    #[test]
+    fn computes_solution() {
+        let (initial, rules) = parse_input(INPUT);
+        let rules = rules
+            .iter()
+            .map(|(pair, c)| ((pair.as_bytes()[0] as char, pair.as_bytes()[1] as char), *c))
+            .collect::<HashMap<_, _>>();
+        let mut pairs = pairs_from_str(&initial);
+        for _i in 1..=40 {
+            pairs = step(pairs, &rules);
+        }
+        let occ = count_occurrences(&pairs);
+        let max = occ.iter().map(|(_, count)| count).max().unwrap();
+        let min = occ.iter().map(|(_, count)| count).min().unwrap();
+        assert_eq!(2516901104210, max - min);
+    }
+
+    #[test]
+    fn creates_initial_map() {
+        let (initial, _rules) = parse_input(EXAMPLE_INPUT);
+        let map = pairs_from_str(&initial);
+        let expected: PairMap = [
+            ('N', [('C', 1), ('N', 1)].into_iter().collect()),
+            ('B', [('@', 1)].into_iter().collect()),
+            ('C', [('B', 1)].into_iter().collect()),
+        ]
+        .into_iter()
+        .collect();
+        assert_eq!(expected, map);
+    }
+
+    #[test]
+    fn creates_larger_initial_map() {
+        let map = pairs_from_str("NCNBCHB".into());
+        let expected: PairMap = [
+            ('N', [('C', 1), ('B', 1)].into_iter().collect()),
+            ('C', [('N', 1), ('H', 1)].into_iter().collect()),
+            ('H', [('B', 1)].into_iter().collect()),
+            ('B', [('C', 1), ('@', 1)].into_iter().collect()),
+        ]
+        .into_iter()
+        .collect();
+        assert_eq!(expected, map);
+    }
+
+    #[test]
+    fn creates_even_larger_initial_map() {
+        let map = pairs_from_str("NBBBCNCCNBBNBNBBCHBHHBCHB".into());
+        let expected: PairMap = [
+            ('N', [('C', 1), ('B', 4)].into_iter().collect()),
+            (
+                'B',
+                [('B', 4), ('C', 3), ('N', 2), ('H', 1), ('@', 1)]
+                    .into_iter()
+                    .collect(),
+            ),
+            ('C', [('N', 2), ('C', 1), ('H', 2)].into_iter().collect()),
+            ('H', [('B', 3), ('H', 1)].into_iter().collect()),
+        ]
+        .into_iter()
+        .collect();
+        assert_eq!(expected, map);
     }
 }
